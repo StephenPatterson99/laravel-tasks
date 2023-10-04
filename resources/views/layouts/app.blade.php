5a4c6fd39278b7cbb6e7abc43b8dfd82b5cf85ba
StephenPatterson99 <61993772+StephenPatterson99@users.noreply.github.com>
2023-10-04 12:30:02 +0100

    Changed page title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Quickstart - Task Manager</title>
+    <title>Laravel Quickstart - Task Manager for Stephen Patterson</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
